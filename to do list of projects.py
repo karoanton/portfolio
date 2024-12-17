5767d7e346ca7e3da1e7be307949d2599b7ff1cc
karoanton <152211728+karoanton@users.noreply.github.com>
2024-12-17 17:54:58 -0500

    changed files

---
+++ to do list of projects.py
@@ -0,0 +1,4 @@
+"""
+Language Modeling: train a language model on a single text corpus
+Corpus Analysis: analyze linguistic features (frequency analysis, n-gram analysis, POS tagging) in a text corpus
+"""
