d2e3239baabad057e1aee8ea043ec2befb5be57c
Karoline Antonsen <152211728+karoanton@users.noreply.github.com>
2024-12-27 19:19:20 -0500

    Update to do list of projects.py

--- to do list of projects.py
+++ to do list of projects.py
@@ -2,6 +2,7 @@
 Linguistic Projects:
 Language Modeling: train a language model on a single text corpus
 Corpus Analysis: analyze linguistic features (frequency analysis, n-gram analysis, POS tagging) in a text corpus
+Cipher: Create a cipher to deliver secret messages with
 
 Hobby Projects:
 Crochet Stash Buster Project Generator: Input amount of yarn (yardage/meterage or weight) and yarn weight (thickness) and output list of projects that can be completed with that amount of yarn.
