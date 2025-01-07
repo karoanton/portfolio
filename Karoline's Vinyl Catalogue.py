449faf5dde298545ef9aa4968809bb8a3abb1447
karoanton <152211728+karoanton@users.noreply.github.com>
2025-01-07 18:50:13 -0500

    updates to vinyl catalogue

--- Karoline's Vinyl Catalogue.py
+++ Karoline's Vinyl Catalogue.py
@@ -6,21 +6,7 @@ print(catalogue.head())
 print(catalogue.info())
 
 
-alanis = ["Alanis Morissette", "Jagged Little Pill", 1995]
-beaches = ["Beaches, The", "Blame My Ex", 2023]
+catalogue = catalogue.drop("Unnamed: 0", axis=1, inplace=True)
 
 
-catalogue.loc[len(catalogue)] = alanis
-print(catalogue.tail())
-
-
-catalogue.loc[len(catalogue)] = beaches
-print(catalogue.tail())
-
-
-catalogue = catalogue.sort_values("Artist")
-catalogue = catalogue.reset_index(drop=True)
-print(catalogue.head(n=10))
-
-
-catalogue.to_csv("Karoline's Vinyl Catalogue.csv")
+catalogue.to_csv("Karoline's Vinyl Catalogue.csv", index=False)
